108a31b89243eef0b2ae0c0c6ac3ee58944d5cc8
mohamedaldahoul <ma811forever@gmail.com>
2019-08-27 10:29:36 +0200

    github GraphQL API

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 yarn init -y
 
+yarn add -D nodemon
+
 yarn add apollo-server graphql
 
 yarn add graphql
+
+
+
+for client 
+
+npm install apollo-boost @apollo/react-hooks graphql --save 
+
+yarn add - D apollo-boost @apollo/react-hooks graphql
+
+yarn add -D apollo-boost react-apollo graphql
+
+npm install apollo-boost react-apollo graphql --save
+
--- src/index.js
+++ src/index.js
@@ -4,13 +4,66 @@ const typeDefs = gql`
 
   type Query {
     hello: String!
+    user: User
   }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
+  type RegisterResponse {
+    errors: [Error]!
+    user: User
+  }
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int
+    
+  }
+ 
+  type Mutation {
+    register(userInfo: UserInfo!) : RegisterResponse!
+    login(userInfo: UserInfo!) : Boolean!
+  }
+
 `;
 
 const resolvers = {
   Query:{
-    hello:()=> 'hello world'
-  }  
+    hello:()=> 'hello world',
+    user: () => ({
+      id: 1,
+      username: "Mohamed" 
+    }),
+  },
+
+  Mutation: {
+    login: () => true,
+    register: () => ({
+      errors: [
+        {
+          field: "username",
+          message: "NO GOOD"
+        },
+        {
+          field: "username2",
+          message: "NO GOOD 2"
+        },
+        null
+      ],
+      user: {
+        id: 1,
+        username: "Mohamed"
+      }
+    })
+  }
 }
 
 const server = new ApolloServer({typeDefs, resolvers})
